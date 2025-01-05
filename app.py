d127e2d8b096a110bb9345ed821dff82d0227b9f
Zakaria_GOUNDIAM <65202415+Baxx95@users.noreply.github.com>
2025-01-05 22:35:17 +0100

    Add files via upload
    my_little_care-render commit

--- app.py
+++ app.py
@@ -66,51 +66,52 @@ def tab_input_v2(desc):
     nombre_de_mots = len(desc)
     return [desc.count(mot)/nombre_de_mots if nombre_de_mots != 0 else 0 for mot in mots_elements]
 #-------------------------------------------------------
-img = Image.open("new_logo.png") 
-st.image(img, width=300)
+img = Image.open("my_little_care.jpg") 
+st.image(img, width=200)
 
 #-------------------------------------------------------
+
 st.subheader("Informations personnelles :")
 age = st.selectbox(
         'Quel âge avez-vous ?',
-         list(range(17,105)))
-
+         list(range(1,105)))
+'vous avez choisi: ', age
 genre = st.radio(
-        'Quel est votre sexe ? ',
+        'Quel est votre sexe ?',
         ("Femme", "Homme", "Non Binaire"))
-
 Nom=st.text_input('Nom : ')
-
-Prenom=st.text_input('Prénom : ')
-
-Email=st.text_input('Email : ')
+"Nom : ", Nom
+Prenom=st.text_input('Prenom : ')
+"Prenom : ", Prenom
+num_tel=st.text_input('Numero de téléphone : ')
+'Numero de téléphone : ', num_tel
 
 #-------------------------------------------------------
 st.subheader("État actuel de la chevelure :")
 rep1 = st.selectbox(
-        'Comment sont vos cheveux naturellement ?',
-         ['Caucasiens', 'Métissés','Crépus'])
-
+        'Comment sont vos cheuveux naturellement ?',
+         ['Lisses', 'Ondulés','bouclés', 'frisés'])
+'vous avez choisi: ', rep1
 rep2 = st.selectbox(
-        'Vos cheveux sont actuellement :',
-         ['Lissés', 'Colorés','Permanentés', 'Naturels', 'Méchés'])
-
+        'Vos cheuveux sont actuellement :',
+         ['Lissés', 'colorés','permanentés', 'Naturels', 'Méchés'])
+'vous avez choisi: ', rep2
 rep3 = st.selectbox(
-        "Quelle est l’épaisseur de vos cheveux ?",
-         ['Fins','Cheveux moyennement épais','Cheuveux épais'])
- 
+        "Quel est l'épaisseur de vos cheveux ?",
+         ['cheveux pas epais','cheveux moyennement epai','cheuveux epai'])
+'Vous avez choisi: ', rep3  
 rep4 = st.selectbox(
         "Quelle est la longueur de vos cheveux ?",
-         ['Longs','Courts','Aux épaules'])
-
-rep5 = st.multiselect(
-    "Comment est votre cuir chevelu ? \nSélectionnez jusqu’à 2 :",
-         ['Sec', 'Gras','Pelliculaire', 'Irrité', 'Sain'])
-   
+         ['Long','Court','Au épaules'])
+'Vous avez choisi: ', rep4
+rep5 = st.selectbox(
+        'Comment est votre cuir chevelu ?',
+         ['Sec', 'Gras','Pelliculaire', 'irrité', 'Sain'])
+'Vous avez choisi: ', rep5   
 rep6 = st.selectbox(
         'Quelle est la porosité de vos cheveux ?',
-         ['Faible porosité','Porosité normale','Forte porosité'])
-   
+         ['faible porosité','porosité normale','forte porosité'])
+'Vous avez choisi: ', rep6   
 rep7 = st.radio(
         'Vous a-t-on diagnostiqué une maladie du cuir chevelu ?',
         ("Oui", "Non"))
@@ -118,88 +119,74 @@ if rep7 == "Oui":
     rep8 = st.selectbox(
         'De quelle maladie s’agit-il ?',
          ['Pelade','Alopécie','Kyste'])
+    'Vous avez choisi: ', rep8 
 
-image_cheveux = st.file_uploader("Envoyez - nous une photo de vos cheveux pour un diagnostic plus précis (Optionnel)")
+image_cheveux = st.file_uploader("Envoyez- nous une photo de vos cheveux pour un diagnostic plus précis (Optionnel)")
 if image_cheveux is not None:
     # To read file as bytes:
     bytes_data = image_cheveux.getvalue()
-    #st.write(bytes_data)  
+    st.write(bytes_data)
+    
 
 #-----------------------------------------------------------
 st.subheader("Routine capillaire ")
 rep9 = st.selectbox(
-        'Combien de fois lavez-vous vos cheveux ?',
-         ['Tous les jours','Tous les 2-3 jours', '1 fois par semaine'])
-
+        'A quelle fréquence vous lavez vous les cheveux ?',
+         ['tous les jours','tous les 2-3 jours', '1 fois par semaine'])
+'Vous avez choisi: ', rep9
 rep10 = st.selectbox(
         'Qu’est ce que vous utilisez généralement pour coiffer vos cheveux ?',
-         ['Gel', 'Fer à lisser', 'Mousse', 'Fer à boucler', 'Laque', 'Spray'])
-
+         ['gel', 'fer à lisser', 'mousse', 'fer à boucler', 'laque', 'spray'])
+'Vous avez choisi: ', rep10
 rep11 = st.selectbox(
         'Sélectionnez l’un des traitements que vous avez actuellement ou que vous prévoyez d’avoir dans les prochaines semaines',
-         ['Lait', 'Masque', 'Huiles', 'Soins sans rinçage', 'Shampooing sec', 'Après shampooing'])
-
+         ['lait', 'masque', 'huiles', 'soins sans rinçage', 'shampooing sec', 'après shampooing'])
+'Vous avez choisi: ', rep11
 rep12 = st.selectbox(
         'Colorez vous ou décolorez-vous vos cheveux ?',
-         ['Parfois','Souvent','Non'])
-
+         ['parfois','souvent','non'])
+'Vous avez choisi: ', rep12
 #-----------------------------------------------------------
 st.subheader("Objectifs")
 rep13 = st.multiselect(
     'Sélectionnez jusqu’à 3 objectifs:',
     ['Réparation','Hydratation','Protection de la couleur','Boucles rebondies',
-    'Soin du cuir chevelu','Lissage','Volume'])
-
+    'Soin du cuir chevelu','Lissage','Volume'],)
+'You selected:', rep13
 #-----------------------------------------------------------
 st.subheader("Futurs soins")
 rep14 = st.selectbox(
-        'Quels produits aimeriez-vous ajouter à votre routine ?',
-         ["Soin de nuit","Shampoing sec","Sérum cuir chevelu","Huile pour cheveux","Lait"])
-
+        'Quels produits voudriez-vous inclure dans votre routine ?',
+         ["Soin de nuit", "Shampoing sec" ,"Sérum cuir chevelu","Huile pour cheveux", "Lait"])
+'Vous avez choisi: ', rep14
 rep15 = st.selectbox(
         'Quel budget avez-vous par mois ?',
          ["10 €", "Entre 10 € - 30 €" ,"Entre 30 € - 50 €","+50 €"])
-
+'Vous avez choisi: ', rep15
 #-----------------------------------------------------------
 st.subheader("Lifestyle")
 rep16 = st.selectbox(
         'Où vivez-vous ?',
-         ["Campagne","Urbain","Péri-urbain"])
-
+         ["Campagne", "Urbain" ,"Péri-urbain"])
+'Vous avez choisi: ', rep16
 rep17 = st.selectbox(
         'Quel est le temps moyen dans votre région ?',
-         ["Ensoleillé","Neigeux","Pluvieux","Nuageux"])
-
+         ["Ensoleillé", "Neigeux" ,"Pluvieux","Nuageux"])
+'Vous avez choisi: ', rep17
 rep18 = st.selectbox(
         'Comment définiriez-vous votre alimentation ?',
-         ["Saine","Junkfood","Flexitarien","Végétarien","Végétalien"])
-
+         ["Saine", "Junkfood" ,"Fléxitarien","Végétarien", "Végétalien"])
+'Vous avez choisi: ', rep18
 rep19 = st.selectbox(
         'Vous évoluez dans un environnement :',
          ["Stressant", "Calme" ,"Fatiguant"])
-
+'Vous avez choisi: ', rep19
 rep20 = st.radio(
         'Êtes-vous enceinte ou allaitante ?',
         ("Oui", "Non"))
 
-#     option = 'liss' if option=='Lisses' else 'ondulé' if option=='Ondulés' else 'boucl' if option=='bouclés' else 'fri'
-#     option1 = 'sec' if option1=='Sec' else 'gra' if option1=='Gras' else 'pellicul' if option1=='Pelliculaire' else 'irrit' if option1=='irrité' else option1
-inputs = [rep1,rep2,rep3,rep4,rep5,rep6,rep7,rep9,rep10,rep11,rep12,rep13,rep14,rep15,rep16,rep17,rep18,rep19,rep20]
-
-def diagnostic(rep1,rep5,rep6):
-    if rep1 == 'Crépus' and 'Sec' in rep5 and rep6 == 'faible porosité':
-        rep = "Vous semblez avoir un problème d'hydratation et de production ou de rétention de sébum.\n"
-    elif rep1 == 'Crépus' and 'Gras' in rep5 and rep6 == 'faible porosité':
-        rep = "Vous semblez avoir un problème d'absortion et de rétention de corps gras externe à la surface de votre fibre capillaire.\n"
-    elif rep1 == 'Crépus' and ('Sec' in rep5 and 'Pelliculaire' in rep5) and rep6 == 'faible porosité':
-        rep = "Vous semblez avoir un problème de production de sébum et d'absortion de corps gras externe et d'eau à la surface de votre fibre capillaire.\n"
-    elif rep1 == 'Crépus' and ('Gras' in rep5 and 'Pelliculaire' in rep5) and rep6 == 'faible porosité':
-        rep = "Vous semblez avoir un problème de production de sébum et d'absortion de corps gras externe et d'eau à la surface de votre fibre capillaire.\n"
-    else:
-        rep = "Vous semblez ne pas avoir un problème particulier, mais un objectif à atteindre.\n"
+inputs = [rep1,rep2,rep3,rep4,rep5,rep6,rep7,rep8,rep9,rep10,rep11,rep12, rep13,rep14,rep15,rep16,rep17,rep18,rep19,rep20]
     
-    return rep
-
 if st.button('Validez'):
         d={}
         for i in tab_prod.index:
@@ -215,8 +202,12 @@ if st.button('Validez'):
         
         prods = ch[:-2].split(',')
         prods = [typ.strip() for typ in prods]
+
         nb_prod = df_data[df_data.type_produit_traite == prods[0]].shape[0] + df_data[df_data.type_produit_traite == prods[1]].shape[0]
 
-        st.write(diagnostic(rep1,rep5,rep6))
-        st.write("Nous avons dans notre base d'articles", nb_prod, " produits de type ",prods[0]," et ", prods[1]," qui vous permettront d'atteindre vos objectifs.\nVeuillez prendre rendez-vous avec un de nos experts pour plus de détails sur les produts et leurs utilisations.")
-        st.write("N'hésitez pas de consulter notre site [MyLittleCare](https://my-little-care.webflow.io/#Produit)")
+        st.write("Nous avons dans notre base d'articles", nb_prod, "produits de type ",ch[:-2]," , qui vous permettront d'atteindre vos objectifs.\nVeuillez prendre rendez-vous avec nos experts pour plus de détails sur les produts et leurs utilisations.")
+        
+
+
+
+
